43b748215227c7f259a9fd9bb213c4d4abb56cd2
Ivan Notaroš <nothke@gmail.com>
2016-05-16 17:02:26 +0200

    Imported DOTween, started working on cockpit

---
+++ Assets/Scripts/CockpitMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CockpitMenu : MonoBehaviour
+{
+
+    public static CockpitMenu e;
+    void Awake() { e = this; }
+
+    public class CockpitSegment
+    {
+        
+
+    }
+
+    public void Update()
+    {
+
+
+
+
+    }
+
+    void ReadInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad8)) // TODO: convert keys to buttons
+            Navigate(1, 0);
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            Navigate(-1, 0);
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+            Navigate(0, -1);
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+            Navigate(0, 1);
+    }
+
+    public void Navigate(int up, int right)
+    {
+
+
+
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public static Menu e;
+    void Awake() { e = this; }
+
+
+}
