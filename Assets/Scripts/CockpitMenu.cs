2ee5bb6b786f3499b3e453b34584a927b8068caf
Ivan Notaroš <nothke@gmail.com>
2016-05-25 19:25:58 +0200

    Changed how Norman terminal works, no more coroutines, pages handled via TerminalPage objects

--- Assets/Scripts/CockpitMenu.cs
+++ Assets/Scripts/CockpitMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using DG.Tweening;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CockpitMenu : MonoBehaviour
 {
@@ -56,6 +57,7 @@ public class CockpitMenu : MonoBehaviour
             case Module.Navigation:
                 break;
             case Module.NormanTerminal:
+                TerminalMoveCursor(by);
                 break;
             default:
                 break;
@@ -82,21 +84,18 @@ public class CockpitMenu : MonoBehaviour
             case Module.Navigation:
                 break;
             case Module.NormanTerminal:
+                TerminalEnter();
                 break;
             default:
                 break;
         }
     }
 
-
-    public class Systems
-    {
-
-    }
-
     public Transform selector;
     int sysSelAt;
     public HUDSwitch[] sysSwitches;
+
+    int curX, curY;
 
     public void SystemsSelector(int by)
     {
@@ -107,12 +106,13 @@ public class CockpitMenu : MonoBehaviour
         selector.position = s.transform.position;
     }
 
-    int Repeat(int value, int max)
-    {
-        if (value < 0) value = max;
-        if (value > max) value = 0;
+    #region Start and Update
 
-        return value;
+    void Start()
+    {
+        InitTerminalPages();
+
+        DisplayTerminalPage(terminalPages[0]);
     }
 
     public void Update()
@@ -120,6 +120,25 @@ public class CockpitMenu : MonoBehaviour
         ReadInput();
 
         UpdateGauges();
+    }
+
+    void ReadInput()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+            NavigateToSegment(1);
+        if (Input.GetKeyDown(KeyCode.Keypad6))
+            NavigateToSegment(-1);
+
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+            NavigateModule(1);
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            NavigateModule(-1);
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            Enter();
+        }
     }
 
     void UpdateGauges()
@@ -132,23 +151,11 @@ public class CockpitMenu : MonoBehaviour
         }
     }
 
-    int curX, curY;
 
-    void ReadInput()
-    {
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-            NavigateToSegment(1);
-        if (Input.GetKeyDown(KeyCode.Keypad6))
-            NavigateToSegment(-1);
 
-        if (Input.GetKeyDown(KeyCode.Keypad8))
-            NavigateModule(1);
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-            NavigateModule(-1);
+    
 
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
-            Enter();
-    }
+    #endregion
 
     int activeModuleInt;
 
@@ -165,26 +172,221 @@ public class CockpitMenu : MonoBehaviour
         activeModule = (Module)activeModuleInt;
     }
 
-    void Start()
+
+
+    //--------
+    // NORMAN
+    //--------
+
+    public class TerminalPage
     {
-        //StartCoroutine(PlayRecord(introRecord));
+        public enum Type { Menu, Record, Update, Terminator };
+        public Type type;
+        public string name;
+        public string next;
+        public string[] lines;
+
+        public TerminalPage(string name, Type type, string[] lines, string next)
+        {
+            this.name = name;
+            this.type = type;
+            this.lines = lines;
+            this.next = next;
+        }
+    }
+
+    List<TerminalPage> terminalPages = new List<TerminalPage>();
+
+    public void InitTerminalPages()
+    {
+        terminalPages.Add(new TerminalPage(
+            "INTRO",
+            TerminalPage.Type.Record,
+            new string[]
+            {
+                "88888888888JLOINNJUUUTTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXCVXCVXCBBBBBBBBBBBOXOXOXOXOOXOXOXOXOXXXXXXXXXXXXX",
+                "Hello\nI am Norman your onboard assistant\nHere to serve you\n\nEnter to continue",
+                "Due to limited memory space and parsing in a harsh environment of space",
+                "If this is your first flight I will take through an introduction so please read carefully",
+                "I warmly recommend using keypad for navigating the ship\nTry it out and return to continue",
+                "Standardly keys 4 and 6 will take you to other modules while 8 and 2 is used for selection",
+                "But its not yet time to get comfy and fly right away\nMake sure to read the manual first",
+                "The main menu is on the next page\nUse 8 and 2 to select and enter to access"
+                    //"Got comfy\nThe menu is on the next page\nCheck the manual and you can now fly"
+            },
+            "MAIN"));
+
+        // MAIN MENU
+
+        terminalPages.Add(new TerminalPage(
+            "MAIN",
+            TerminalPage.Type.Menu,
+            new string[]
+            {
+                "BODY INFO",
+                "MISSION",
+                "INTRO",
+                "CONTROLS",
+                "MANUAL",
+                "CHECKLISTS",
+                "CREDITS"
+            },
+            ""));
+
+        // MANUAL
+
+        terminalPages.Add(new TerminalPage(
+            "MANUAL",
+            TerminalPage.Type.Menu,
+            new string[]
+            {
+                "STARTUP",
+                "FLIGHT",
+                "SYSTEMS",
+                "NAVIGATION",
+                "MAIN"
+            },
+            ""));
+
+        terminalPages.Add(new TerminalPage(
+            "STARTUP",
+            TerminalPage.Type.Record,
+            new string[]
+            {
+                "Startup Procedure\nThis manual will show you how to perform a start up sequence",
+                "The whole start up is handled in the system module which is second on the right of the cockpit",
+                "First, we need to turn main batteries on, it is the BATT switch",
+                "The voltage indicator on the top right should now show peak voltage, at the top of green bar",
+                "If it does not, please refer to the electrics manual",
+                "Now we need to start the primary engines. Begin by letting the fuel in",
+                "As the fuel valve is open, you should notice a tiny flow on the flow spinner at the low center",
+                "But the flow is not enough for the engines to pick up the pressure. Turn the PUMP on",
+                "Now the pump is forcing the fuel and you can see the flow ind is spinning fast",
+                "Also notice that running the pump has increased load on the battery and lowered the voltage",
+                "Now we can begin ignition. On the right of the engine panel are 3 buttons, press one of the 3",
+                "As the engine is being started, you can see the voltage surge, it takes a lot of power",
+                "Once the engine has started, it is pulling enough fuel by itself that the pump is no longer required",
+                "You can turn off the pump and then start the other 2 engines"
+
+            },
+            "MANUAL"));
+
+        terminalPages.Add(new TerminalPage(
+            "ELECTRICS",
+            TerminalPage.Type.Record,
+            new string[]
+            {
+                "REC",
+                "Electrics manual\nThis manual will explain the electrics system on the ship",
+                "Intro: There are 2 power sources on the ship, primary and backup cell",
+                "The primary power source on the ship is the fuel cell aka battery running on unobtainium",
+                "In the event of a primary battery failure, a backup cell will activate..",
+                "..and power critical elements like this terminal and life support",
+                "There are no other generators in this version of the ship",
+
+                "To turn the primary power source, use the switch titled BATT in the systems module",
+                "Once it is active the voltage meter on the top right will indicate current voltage",
+
+
+                "Load:\nEvery device running using the onboard electricity is applying a load",
+                "..and the voltage will drop",
+                "If the voltage drops to the red zone, the power will automatically turn off..",
+                "..to prevent devices from operating at a not designated voltage",
+                "..and will resume if the voltage returns to the optimal zone",
+                "In the case of overvoltage, the fuse will blow",
+                "So, manage the devices properly to keep the voltage at optimium levels..",
+                "And keep an eye on the voltage meter",
+                "End of electrics manual"
+            },
+            "MANUAL"));
+
+
+
+
+        // CHECKLISTS
+
+        terminalPages.Add(new TerminalPage(
+            "CHECKLISTS",
+            TerminalPage.Type.Menu,
+            new string[]
+            {
+                "START UP",
+                "SHUT DOWN",
+                "APPROACH",
+                "MAIN"
+            },
+            ""));
+
+        terminalPages.Add(new TerminalPage(
+            "START UP",
+            TerminalPage.Type.Menu,
+            new string[]
+            {
+                "REC",
+                "Systems module:\n" +
+                "1.BATT - on\n" +
+                "2.FUEL - on\n" +
+                "3.PUMP - on\n" +
+                "4.ENGINE 1 - on\n" +
+                "5.PUMP - off\n" +
+                "6.ENGINE 2 - on\n" +
+                "7.ENGINE 3 - on\n"
+            },
+            "CHECKLISTS"));
+
+        terminalPages.Add(new TerminalPage(
+    "SHUT DOWN",
+    TerminalPage.Type.Menu,
+    new string[]
+    {
+                "REC",
+                "Systems module:\n" +
+                "1.FUEL - cut\n" +
+                "1.BATT - off\n"
+    },
+    "CHECKLISTS"));
+    }
+
+    public TerminalPage GetTerminalPage(string key)
+    {
+        foreach (var t in terminalPages)
+        {
+            if (t.name == key)
+                return t;
+        }
+
+        Debug.Log("Unknown Page");
+
+        return null;
     }
 
     public Text normanText;
 
-    string[] introRecord =
-    {
-        "88888888888JLOINNJUUUTTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXCVXCVXCBBBBBBBBBBBOXOXOXOXOOXOXOXOXOXXXXXXXXXXXXX",
-        "Hello\nI am Norman your onboard assistant\nHere to serve you\n\nEnter to continue",
-        "Due to limited memory space and parsing in a harsh environment of space",
-        "If this is your first flight I will take through an introduction so please read carefully",
-        "I warmly recommend using keypad for navigating the ship\nTry it out and return to continue",
-        "Standardly keys 4 and 6 will take you to other modules while 8 and 2 is used for selection",
-        "But its not yet time to get comfy and fly right away\nMake sure to read the manual first",
-        "The main menu is on the next page\nUse 8 and 2 to select and enter to access"
-            //"Got comfy\nThe menu is on the next page\nCheck the manual and you can now fly"
-    };
+    TerminalPage currentTerminalPage;
 
+    public void DisplayTerminalPage(TerminalPage tp)
+    {
+        if (tp == null)
+            DisplayErrorPage(); //TODO
+
+        currentTerminalPage = tp;
+
+        if (tp.type == TerminalPage.Type.Menu)
+            DisplayMenu(tp.lines);
+
+        if (tp.type == TerminalPage.Type.Record)
+        {
+
+        }
+
+    }
+
+    void DisplayErrorPage()
+    {
+
+    }
+
+    [System.Obsolete]
     IEnumerator PlayRecord(string[] record)
     {
         if (record == null) { Debug.LogWarning("Record is null"); yield break; }
@@ -202,6 +404,196 @@ public class CockpitMenu : MonoBehaviour
         DisplayMenu();
     }
 
+
+
+    void DisplayMenu(string[] menuLines)
+    {
+        StopAllCoroutines();
+
+        string str = "";
+
+        for (int i = 0; i < menuLines.Length; i++)
+        {
+            if (i == cursorPos)
+                str += "8";
+            else
+                str += " ";
+
+            str += menuLines[i] + "\n";
+        }
+
+        WriteText(str);
+
+        //StartCoroutine(MenuSelectLoop(menuLines));
+    }
+
+    /*
+    IEnumerator MenuSelectLoop(string[] menuLines)
+    {
+        yield return null;
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                MoveCursor(1);
+                DisplayMenu();
+                break;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad8))
+            {
+                MoveCursor(-1);
+                DisplayMenu();
+                break;
+            }
+
+            if (pressEnter)
+            {
+                string menu = mainMenuLines[cursorPos];
+
+                switch (menu)
+                {
+                    case "body info": StartCoroutine(BodyInfo()); break;
+                    case "play intro": StartCoroutine(PlayRecord(introRecord)); break;
+                    case "credits": Credits(); break;
+                    default: StartCoroutine(BodyInfo()); break;
+                }
+
+                yield break;
+            }
+
+            yield return null;
+        }
+        yield return null;
+
+    }*/
+
+    //public enum ScreenType { Menu, Record, Update }
+
+    /*
+    void TerminalUpdate()
+    {
+
+        string[] nextLines = null;
+
+        // navigate up
+
+        // navigate down
+
+        if (pressEnter)
+        {
+            ScreenType screenType = ScreenType.Menu;
+
+            switch (screenType)
+            {
+                case ScreenType.Menu:
+                    DisplayMenu(nextLines);
+                    break;
+                case ScreenType.Record:
+                    PlayRecord(manualLines);
+                    break;
+                case ScreenType.Update:
+                    StartCoroutine(BodyInfo());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }*/
+
+    //ScreenType nextScreenType = ScreenType.Record;
+    //string[] nextScreenLines;
+    //string[] currentScreenLines;
+
+    void TerminalMoveCursor(int by)
+    {
+        if (currentTerminalPage.type != TerminalPage.Type.Menu)
+            return;
+
+        cursorPos += by;
+
+        int maxPos = mainMenuLines.Length - 1;
+        cursorPos = Repeat(cursorPos, by);
+
+        // refresh terminal
+        DisplayMenu(currentTerminalPage.lines);
+    }
+
+    [System.Obsolete("Use TermnalMoveCursor instead")]
+    void MoveCursor(int by)
+    {
+        cursorPos += by;
+
+        int maxPos = mainMenuLines.Length - 1;
+
+        if (cursorPos > maxPos)
+            cursorPos = 0;
+
+        if (cursorPos < 0)
+            cursorPos = maxPos;
+    }
+
+    int atRecordPage;
+
+    void TerminalEnter()
+    {
+        switch (currentTerminalPage.type)
+        {
+            case TerminalPage.Type.Menu:
+                DisplayTerminalPage(GetTerminalPage(currentTerminalPage.lines[atRecordPage]));
+                atRecordPage = 0;
+                break;
+            case TerminalPage.Type.Record:
+                NextRecordPage();
+                break;
+            case TerminalPage.Type.Update:
+                StartCoroutine(BodyInfo());
+                break;
+            case TerminalPage.Type.Terminator:
+                DisplayTerminalPage(terminalPages[1]); // DIRTY - pointing to main menu
+                break;
+            default:
+                break;
+        }
+    }
+
+    void NextRecordPage()
+    {
+        TerminalPage tp = currentTerminalPage;
+
+        if (atRecordPage >= tp.lines.Length + 1)
+        {
+            DisplayTerminalPage(GetTerminalPage(tp.next));
+            return;
+        }
+
+        WriteText(tp.lines[atRecordPage]);
+        atRecordPage++;
+    }
+
+    [System.Obsolete]
+    void DisplayMenu()
+    {
+        StopAllCoroutines();
+
+        string str = "";
+
+        for (int i = 0; i < mainMenuLines.Length; i++)
+        {
+            if (i == cursorPos)
+                str += "8";
+            else
+                str += " ";
+
+            str += mainMenuLines[i] + "\n";
+        }
+
+        WriteText(str);
+
+        StartCoroutine(MenuLoop());
+    }
+
+    [System.Obsolete]
     IEnumerator MenuLoop()
     {
         yield return null;
@@ -221,14 +613,14 @@ public class CockpitMenu : MonoBehaviour
                 break;
             }
 
-            if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+            if (pressEnter)
             {
-                string menu = menus[cursorPos];
+                string menu = mainMenuLines[cursorPos];
 
                 switch (menu)
                 {
                     case "body info": StartCoroutine(BodyInfo()); break;
-                    case "play intro": StartCoroutine(PlayRecord(introRecord)); break;
+                    //case "play intro": StartCoroutine(PlayRecord(introRecord)); break;
                     case "credits": Credits(); break;
                     default: StartCoroutine(BodyInfo()); break;
                 }
@@ -241,24 +633,7 @@ public class CockpitMenu : MonoBehaviour
         yield return null;
     }
 
-    int cursorPos;
-
-    void MoveCursor(int by)
-    {
-        cursorPos += by;
-
-        int maxPos = menus.Length - 1;
-
-        if (cursorPos > maxPos)
-            cursorPos = 0;
-
-        if (cursorPos < 0)
-            cursorPos = maxPos;
-    }
-
-    string displayStr;
-
-    IEnumerator ReturnToMenuLoop()
+    IEnumerator ReturnToMainMenu()
     {
         yield return null;
 
@@ -270,9 +645,19 @@ public class CockpitMenu : MonoBehaviour
         DisplayMenu();
     }
 
+    bool pressEnter;
+
+    int cursorPos;
+
+
+
+    string displayStr;
+
+
+
     IEnumerator BodyInfo()
     {
-        StartCoroutine(ReturnToMenuLoop());
+        StartCoroutine(ReturnToMainMenu());
 
         while (true)
         {
@@ -311,40 +696,39 @@ public class CockpitMenu : MonoBehaviour
 
     void Credits()
     {
-        StartCoroutine(ReturnToMenuLoop());
+        StartCoroutine(ReturnToMainMenu());
 
         WriteText("Made by:\nIvan Notaros Nothke\n\nMusic by\nMartin Kvale\n\nShader mage\nLeon Denise");
     }
 
-    string[] menus = {
+    string[] mainMenuLines = {
+        "MENU",
         "body info",
         "plane",
         "mission",
         "play intro",
         "controls",
         "manual",
-        "credits"};
+        "checklists",
+        "credits"
+    };
 
-    void DisplayMenu()
+    string[] manualLines =
     {
-        StopAllCoroutines();
+        "MENU",
+        "startup",
+        "flight",
+        "systems",
+        "navigation"
+    };
 
-        string str = "";
-
-        for (int i = 0; i < menus.Length; i++)
-        {
-            if (i == cursorPos)
-                str += "8";
-            else
-                str += " ";
-
-            str += menus[i] + "\n";
-        }
-
-        WriteText(str);
-
-        StartCoroutine(MenuLoop());
-    }
+    string[] checklists =
+    {
+        "MENU",
+        "start up",
+        "shut down",
+        "approach"
+    };
 
     void WriteText(string inStr)
     {
@@ -357,5 +741,13 @@ public class CockpitMenu : MonoBehaviour
     {
         return inStr.ToUpper();
         //return inStr.ToUpper().Replace(" ", "    ");
+    }
+
+    int Repeat(int value, int max)
+    {
+        if (value < 0) value = max;
+        if (value > max) value = 0;
+
+        return value;
     }
 }
